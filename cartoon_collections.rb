28b990e2462b6fbb17cec99ef8116ede921d361b
Agnieszka Brown <abrown2012@icloud.com>
2020-10-24 17:39:24 -0400

    commit tree 5449248a2ca0941632e0bcc500b87d1a9510b97f

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.collect do |name|
+    print "#{names.index(name)+1}. #{name}"
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  collection = []
+  calls.collect do |call|
+    collection << "#{call.capitalize()}!"
+  end   
+  collection 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any?{|call| call.size > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ingredients.detect{|ingredient| cheese_types.include?(ingredient)} 
 end
